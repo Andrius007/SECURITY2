9f994121ac6cb47a6cf10cc347d1958778e70089
Андрей <abramov-hills@mail.ru>
2021-04-21 22:26:51 +0300

    Initial commit

---
+++ src/main/java/spring/com/dao/RoleDAOImpl.java
@@ -0,0 +1,57 @@
+package spring.com.dao;
+
+import org.springframework.stereotype.Repository;
+import spring.com.model.Role;
+import spring.com.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository("roleDAO")
+public class RoleDAOImpl implements RoleDAO {
+
+    private EntityManager entityManager;
+
+    @PersistenceContext
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Role getRoleById(int id) {
+        return entityManager.find(Role.class, id);
+    }
+
+    @Override
+    public List<Role> getAllRoles() {
+        return entityManager.createQuery("select r from Role r").getResultList();
+    }
+
+
+    @Override
+    public Role getRoleByName(String roleName) {
+        return (Role) entityManager.createQuery("select r from Role r where r.roleName= :name")
+                .setParameter("name", roleName)
+                .getSingleResult();
+    }
+
+    @Override
+    public void deleteRole(int id) {
+        entityManager.remove(entityManager.find(User.class, id));
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        entityManager.persist(role);
+
+        return role;
+    }
+
+    @Override
+    public Role updateRole(Role role) {
+        entityManager.merge(role);
+        return role;
+    }
+}
+
---
+++ src/main/java/spring/com/dao/UserDAOImpl.java
@@ -0,0 +1,57 @@
+package spring.com.dao;
+
+import org.springframework.stereotype.Repository;
+import spring.com.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository("userDAO")
+public class UserDAOImpl implements UserDAO {
+
+
+    private EntityManager entityManager;
+
+    @PersistenceContext
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("select u from User u").getResultList();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return (User) entityManager.createQuery("select u from User u where u.email= :email")
+                .setParameter("email", username.toLowerCase())
+                .getSingleResult();
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        entityManager.remove(entityManager.find(User.class, id));
+    }
+
+    @Override
+    public User saveUser(User user) {
+
+        entityManager.persist(user);
+
+        return user;
+    }
+
+    @Override
+    public User updateUser(User user) {
+        entityManager.merge(user);
+        return user;
+    }
+}
